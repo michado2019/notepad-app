a9ca65c22340a1673748b0d7a34e09b036b8aea0
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-06-09 12:49:49 +0100

    home component modified

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -10,8 +10,7 @@ const Home = () => {
           ensure it is only saved on your local machine</h1>
         <p className="homeNoteDetails">
           This is the best note app on the internet, where you can write a note
-          and keep for future reference. Your note is confidencial so we made
-          sure it is only saved on your local machine. You can save your school
+          and keep for future reference. You can save your school
           notes on it. Notes from your devotional can also be saved on it for
           easy access and reference as you go on with your daily activities
         </p>
